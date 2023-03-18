effc418c15b830fc0b8b045e07590d2986bdfaa6
Nastimir <khoroshunov.02@mail.ru>
2023-03-19 00:04:23 +0300

    slider finish

--- src/panels/Slider.jsx
+++ src/panels/Slider.jsx
@@ -1,13 +1,11 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
-import { Panel, PanelHeader, PanelHeaderBack, Text, CellButton, Group, Div, Button, IconButton, Banner, Image } from '@vkontakte/vkui';
-import { Icon16Add, Icon24Add, Icon28ArrowDownToSquareOutline } from '@vkontakte/icons';
+import { Footnote, Panel, PanelHeader, PanelHeaderBack, Text, CellButton, Group, Div, Button, IconButton, Banner, Image } from '@vkontakte/vkui';
+import { Icon16Add, Icon24Add, Icon28ArrowDownToSquareOutline, Icon16MoreVertical } from '@vkontakte/icons';
 import './Mainpage.css';
-import { Title, ZButton } from '../components/styled'
-import Alert from './Alert';
+// import { Title, ZButton, MoreButton } from '../components/styled'
 import { GlobalContext } from '../context';
 import Mainpage from './Mainpage';
-
 const Slider = props => {
   const { go } = useContext(GlobalContext)
 
@@ -20,15 +18,57 @@ const Slider = props => {
         </Text>
       </PanelHeader>
       <Banner
-        before={
-          <Image
-            size={256}
-            src="https://sun9-32.userapi.com/uFzLOK55iY7pC0DHjneEdP9G6gXcXi2Mxj9wVA/wnTmzh_blNM.jpg"
+        mode="image"
+        size='l'
+        background={
+          <div
+            style={{
+              backgroundColor: '#000',
+              backgroundImage:
+                'url(https://sun9-53.userapi.com/m-ygfKiLKLkEMAQVTToO2l9LyC6GgqWoGXpw8A/-zm6_XLECTU.jpg)',
+              backgroundPosition: 'right bottom',
+              backgroundSize: 340,
+              maxwidth: 300,
+              backgroundRepeat: 'no-repeat',
+            }}
           />
         }
-        actions={<Button>Подробнее</Button>}
-      />
-    </Panel>
+        header={<div style={{ opacity: .0, height: 80 }} />}
+        subheader={<div style={{ opacity: .0, height: 80 }} />}
+        asideMode="dismiss"
+
+      ></Banner>
+      <Banner
+
+        mode="image"
+        size='l'
+        background={
+          <div
+            style={{
+              backgroundColor: '#000',
+              backgroundImage:
+                'url(https://sun9-53.userapi.com/m-ygfKiLKLkEMAQVTToO2l9LyC6GgqWoGXpw8A/-zm6_XLECTU.jpg)',
+              backgroundPosition: 'right bottom',
+              backgroundSize: 340,
+              maxwidth: 300,
+              backgroundRepeat: 'no-repeat',
+            }}
+          />
+        }
+        header={<div style={{ opacity: .0, height: 80 }} />}
+        subheader={<div style={{ opacity: .0, height: 80 }} />}
+        asideMode="dismiss"
+
+      ></Banner>
+
+      <div className='New_slide'>
+        <Button size="l" appearance="accent" mode="tertiary" before={<Icon24Add />} >
+          <span className='Button_text'>Добавить слайд</span>
+        </Button>
+      </div>
+
+
+    </Panel >
   )
 };
 
